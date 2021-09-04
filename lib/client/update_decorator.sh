5925e7bc2d788886acd19e97dcd86c2a7afec1a1
Constantin Gahr <constantin.gahr@ipp.mpg.de>
2021-09-04 10:03:56 +0200

    updated decorator to v4.4.2

---
+++ lib/client/update_decorator.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# This script updates decorator to another version. Version is either the git tag
+# or the commit has of the respective release
+#
+# Usage:
+#       ./update_traitlets.sh 4.0.0
+#
+cur_path=`pwd`
+red='\033[0;31m'
+noc='\033[0m'
+
+if [[ $1 == "" ]]; then
+    echo -e "${red}ERROR"
+    echo -e "${noc}No version spefified."
+    echo ""
+    echo "Exiting..."
+    exit 1;
+fi
+
+
+rm -rf /tmp/decorator
+git clone https://github.com/micheles/decorator /tmp/decorator
+
+cd /tmp/decorator
+git checkout $1
+cd $cur_path
+
+git_status=`git status -- decorator`
+git_clean=$(echo -e "On branch update_libraries\nnothing to commit, working tree clean\n")
+
+if [[ $git_status == $git_clean ]]; then
+    rm -rf decorator/
+    mv /tmp/decorator/src/decorator.py .
+    echo $1 > decorator_version
+else
+    echo ""
+    echo -e "${red}ERROR"
+    echo "There are uncommitted changes or untracked files in './decorator.py'."
+    echo ""
+    echo -e "${noc}See 'git status -- decorator.py' for more details."
+fi
+
+rm -rf /tmp/decorator
+
